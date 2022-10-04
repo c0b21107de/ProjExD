4b1395be3416479a3800c10e152fd0a3d713a473
C0B21107 <c0b21107de@edu.teu.ac.jp>
2022-10-04 15:25:18 +0900

    練習7

--- ex02/calc.py
+++ ex02/calc.py
@@ -8,6 +8,12 @@ def button_click(event):
     # tkm.showinfo(txt, f"{txt}ボタンがクリックされました")
     entry.insert(tk.END,f"{txt}")
 
+def button_total(event):
+    total = entry.get()
+    total = eval(total)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,f"{total}")
+
 root = tk.Tk()
 root.geometry("300x500")
 
@@ -27,5 +33,9 @@ for i, num in enumerate(range(9,-1,-1),1):
 plus = tk.Button(root,text="+",font=("",30),width=4,height=2)
 plus.bind("<1>",button_click)
 plus.grid(row=4, column=1)
+
+eq = tk.Button(root, text="=",font=("",30),width=4,height=2)
+eq.bind("<1>",button_total)
+eq.grid(row=4, column=2)
 
 root.mainloop()
