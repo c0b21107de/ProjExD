d46d33cd339eb042a94334bf42af9697490ca010
C0B21107 <c0b21107de@edu.teu.ac.jp>
2022-10-18 15:30:44 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,26 +1,40 @@
 import tkinter as tk
-import maze_maker
+import maze_maker # pra8
+from random import randint
 
-def key_down(event):#pra5
+
+def key_down(event):# pra5
     global key
     key = event.keysym
 
 
-def key_up(event): #pra6
+def key_up(event): # pra6
     global key
     key = ""
 
 
-def main_proc(): #pra7
+def main_proc(): # pra7
     global cx, cy
+    global mx, my
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+    if maze_list[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1
     canv.coords("tori", cx, cy)
     root.after(100,main_proc)
 
@@ -32,20 +46,30 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black") # pra2
     canv.pack()
 
-    tori = tk.PhotoImage(file="fig/5.png") # pra3
+    # pra9
+    maze_list = maze_maker.make_maze(15, 9)
+
+    # pra10
+    maze_maker.show_maze(canv, maze_list)
+
+    # pra11
+    mx, my = 1, 1
+
+    random_image = randint(0,9)
+    tori = tk.PhotoImage(file=f"fig/{random_image}.png") # pra3
     cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag="tori")
 
-    #pra4
+    # pra4
     key = "" # 現在押されているキーを表す
 
-    #pra5
+    # pra5
     root.bind("<KeyPress>", key_down)
     
-    #pra6
+    # pra6
     root.bind("<KeyRelease>", key_up)    
     
-    #pra7
+    # pra7
     main_proc()
 
     root.mainloop()
