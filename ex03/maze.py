afe464669fc8fbd778e22a58cee0936b1a7fc74a
C0B21107 <c0b21107de@edu.teu.ac.jp>
2022-10-18 14:19:25 +0900

    maze

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,5 +6,11 @@ if __name__ == "__main__":
 
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
-    
+
+    tori = tk.PhotoImage(file="fig/5.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag="tori")
+
+    key = ""
+
     root.mainloop()
