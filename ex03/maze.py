8967c2fd7e4d243f2f8f416df91e0ce19987983e
C0B21107 <c0b21107de@edu.teu.ac.jp>
2022-10-18 17:06:24 +0900

    maze

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,8 +3,21 @@ import maze_maker # pra8
 from random import randint
 
 
+def count_up(): # 開始からの時刻を計測する
+    global tmr,jid
+    global mx, my
+    tmr = tmr + 1
+    label["text"] = tmr
+    if mx == random_goal and my == 8:
+        root.after_cancel(jid)
+        jid = None
+        return
+    jid = root.after(1000, count_up)
+
+
 def key_down(event):# pra5
     global key
+    global random_start
     key = event.keysym
 
 
@@ -16,6 +29,10 @@ def key_up(event): # pra6
 def main_proc(): # pra7
     global cx, cy
     global mx, my
+    global tmr
+    if key == "c":
+        mx = random_goal
+        my = 8
     if key == "Up":
         my -= 1
     if key == "Down":
@@ -35,8 +52,12 @@ def main_proc(): # pra7
             mx += 1
         if key == "Right":
             mx -= 1
+    if mx == random_goal and my == 8:
+        canv.coords("tori", cx, cy)
+        return
     canv.coords("tori", cx, cy)
     root.after(100,main_proc)
+
 
 
 if __name__ == "__main__":
@@ -46,19 +67,32 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black") # pra2
     canv.pack()
 
+    label = tk.Label(root, font=("", 30))
+    label.pack()
+
     # pra9
     maze_list = maze_maker.make_maze(15, 9)
+    random_start = randint(1,13)    # スタートとゴールを作成
+    maze_list[0][random_start] = 0
+    maze_list[1][random_start] = 0
+    random_goal = randint(1,7)
+    maze_list[8][random_goal] = 0
+    maze_list[7][random_goal] = 0
 
     # pra10
     maze_maker.show_maze(canv, maze_list)
 
     # pra11
-    mx, my = 1, 1
+    mx, my = random_start, 0
 
-    random_image = randint(0,9)
+    random_image = randint(0,9) # ランダムに画像を変える
     tori = tk.PhotoImage(file=f"fig/{random_image}.png") # pra3
     cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag="tori")
+    
+    tmr = 0
+    jid = None
+    jid = root.after(1000,count_up)
 
     # pra4
     key = "" # 現在押されているキーを表す
