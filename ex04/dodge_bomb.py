26ed366688965c756d628976ee6a9f2380c43dc6
C0B21107 <c0b21107de@edu.teu.ac.jp>
2022-10-25 14:14:47 +0900

    pra

---
+++ ex04/dodge_bomb.py
@@ -0,0 +1,21 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん")
+    scrn_sfc = pg.display.set_mode((1600,900))
+    
+    bg_sfc = pg.image.load("fig/pg_bg.jpg")
+    bg_rct = bg_sfc.get_rect()
+    
+    clock = pg.time.Clock()
+    while True:
+        scrn_sfc.blit(bg_sfc, bg_rct)
+        pg.display.update()
+        clock.tick(1000)
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
