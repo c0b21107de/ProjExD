a6cba8c90e438baee50ecf07c08af989616dc6e4
C0B21107 <c0b21107de@edu.teu.ac.jp>
2022-10-25 14:51:36 +0900

    pra4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,10 +1,12 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     # pra1
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
+    scrn_rct = scrn_sfc.get_rect()
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
 
@@ -13,6 +15,13 @@ def main():
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
+
+    # pra5
+    bomb_sfc = pg.Surface((20,20))
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx, bomb_rct.centery = random.randint(10, scrn_rct.width), random.randint(0, scrn_rct.height)
 
     clock = pg.time.Clock()
     # pra2
@@ -32,6 +41,8 @@ def main():
             tori_rct.centerx -= 1
         if key_states[pg.K_RIGHT]:
             tori_rct.centerx += 1
+        
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
         pg.display.update()
         clock.tick(1000)
 
