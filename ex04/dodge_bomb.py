9d6a8df0a47315c3af716ab05d9845f36c156506
C0B21107 <c0b21107de@edu.teu.ac.jp>
2022-10-25 17:29:30 +0900

    逃げろ！こうかとん

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import sys
 import random
+import time
 
 # pra7
 def check_bound(obj_rct, scr_rct):
@@ -16,6 +17,34 @@ def check_bound(obj_rct, scr_rct):
     if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
         tate = -1
     return yoko, tate
+
+def gameover(): # gameover画面の追加
+    scrn_sfc = pg.display.set_mode((1600,900))
+    scrn_rct = scrn_sfc.get_rect()
+    bg_sfc = pg.image.load("fig/gameover.png")
+    bg_rct = bg_sfc.get_rect()
+    tori_sfc = pg.image.load("fig/8.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 4.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 800, 450
+    clock =pg.time.Clock()
+    while True:
+        scrn_sfc.blit(bg_sfc, bg_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        scrn_sfc.blit(tori_sfc, tori_rct)
+        key_state = pg.key.get_pressed()
+        if key_state[pg.K_c]: # コンテニュー
+            main()
+            return
+        if key_state[pg.K_ESCAPE]:
+            return
+        pg.display.update()
+        clock.tick(1000)
+
+
+        
 
 def main():
     # pra1
@@ -37,10 +66,16 @@ def main():
     pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
     bomb_rct = bomb_sfc.get_rect()
     bomb_rct.centerx, bomb_rct.centery = random.randint(10, scrn_rct.width), random.randint(0, scrn_rct.height)
+    # 爆弾2の追加
+    bomb_sfc2 = pg.Surface((20,20))
+    bomb_sfc2.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc2, (255, 0, 0), (10, 10), 10)
+    bomb_rct2 = bomb_sfc2.get_rect()
+    bomb_rct2.centerx, bomb_rct2.centery = random.randint(10, scrn_rct.width), random.randint(0, scrn_rct.height)
 
     # pra6
     vx, vy = +1, +1
-
+    vx2, vy2 = +1, +1 
     clock = pg.time.Clock()
     # pra2
     while True:
@@ -74,14 +109,44 @@ def main():
         scrn_sfc.blit(tori_sfc, tori_rct)
         
         yoko, tate = check_bound(bomb_rct, scrn_rct)
+        if yoko == -1: # 爆弾の加速
+            yoko -= 0.5
+            tate += 0.5
+
+        if tate == -1: # 爆弾の加速
+            yoko += 0.5
+            tate -= 0.5
+
         vx *= yoko
         vy *= tate
-        bomb_rct.move_ip(vx,vy)
+
+        yoko2, tate2 = check_bound(bomb_rct2, scrn_rct) # 爆弾2の動作
+        if yoko2 == -1: # 爆弾の加速
+            yoko2 -= 0.2
+            tate2 += 0.2
+
+        if tate2 == -1: # 爆弾の加速
+            yoko2 += 0.2
+            tate2 -= 0.2
+        vx2 *= yoko2
+        vy2 *= tate2
+
+        if key_states[pg.K_1]: # 押している間しか見ることができない爆弾の
+            scrn_sfc.blit(bomb_sfc2, bomb_rct2)
+            
+        
+        bomb_rct.move_ip(vx, vy)
+        bomb_rct2.move_ip(vx2, vy2)
         scrn_sfc.blit(bomb_sfc, bomb_rct)
 
         # pra8
-        if tori_rct.colliderect(bomb_rct):
-            return
+        if tori_rct.colliderect(bomb_rct) or tori_rct.colliderect(bomb_rct2): # 爆弾に当たった瞬間gameover画面を表示
+            if key_states[pg.K_p]:
+                pass
+            else:
+                gameover()
+                return
+        
 
         pg.display.update()
         clock.tick(1000)
